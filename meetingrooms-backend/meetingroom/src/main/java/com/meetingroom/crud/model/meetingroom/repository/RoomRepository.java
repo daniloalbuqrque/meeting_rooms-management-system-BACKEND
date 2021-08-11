c74adf0d5d381e796e78d3f9a3a64453827fb897
Danilo Albuquerque <daniloademoraes@gmail.com>
2021-08-11 00:51:11 -0300

    Add the BACK-END files

---
+++ meetingrooms-backend/meetingroom/src/main/java/com/meetingroom/crud/model/meetingroom/repository/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.meetingroom.crud.model.meetingroom.repository;
+
+import com.meetingroom.crud.model.meetingroom.model.Room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
---
+++ meetingrooms-backend/meetingroom/src/test/java/com/meetingroom/crud/model/meetingroom/MeetingroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetingroom.crud.model.meetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
